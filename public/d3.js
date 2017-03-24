a52e919490f3301c6fadc73eceb7669a442aee55
Anna Mefford <annamefford@gmail.com>
2017-03-24 16:11:13 -0600

    first d3 append added

---
+++ public/d3.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var dataset = [ 5, 10, 15, 20, 25 ];
+
+d3.select("body").selectAll("p")
+    .data(dataset)
+    .enter()
+    .append("p")
+    .text(function(d) { return d; })
+    .style("color", "red");
+
+
+console.log(d3.selectAll("p"));
