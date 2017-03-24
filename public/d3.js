4c255285c09c30c8d5ab8f0433bc323944c64494
Anna Mefford <annamefford@gmail.com>
2017-03-24 16:52:20 -0600

    circles added per data set

--- public/d3.js
+++ public/d3.js
@@ -1,13 +1,41 @@
 'use strict';
 
 var dataset = [ 5, 10, 15, 20, 25 ];
-
-d3.select("body").selectAll("p")
+//bar chart
+d3.select("body").selectAll("div")
     .data(dataset)
     .enter()
-    .append("p")
-    .text(function(d) { return d; })
-    .style("color", "red");
+    .append("div")
+    .attr("class", "bar")
+    .style("height", function(d) {
+    return d + "px";
+  });
 
 
 console.log(d3.selectAll("p"));
+//svg
+var w = 500;
+var h = 50;
+
+var svg = d3.select("body")
+      .append("svg")
+      .attr("width", w)
+      .attr("height", h);
+
+var circles = svg.selectAll("circle")
+    .data(dataset)
+    .enter()
+    .append("circle");
+
+    circles.attr("cx", function(d, i) {
+      return (i * 50) + 25;
+    })
+     .attr("cy", h/2)
+     .attr("r", function(d) {
+      return d;
+     })
+     .attr("fill", "yellow")
+     .attr("stroke", "orange")
+     .attr("stroke-width", function(d) {
+      return d/2;
+     });
